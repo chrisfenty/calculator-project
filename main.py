3a76adb225caaa0d4f2eb5ab2432b30eb8d905fb
Christian Adjogbe <Adjosola@gmail.com>
2025-06-05 09:58:37 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+
+import calculator
+
+x = 5
+y = 3
+
+print("Addition:", calculator.add(x, y))
+print("Subtraction:", calculator.subtract(x, y))
+print("Multiplication:", calculator.multiply(x, y))
+print("Division:", calculator.divide(x, y))
